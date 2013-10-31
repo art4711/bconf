3acd34f92a87872c8b812e99f708c33b3f2358f9
Artur Grabowski <art@blahonga.org>
2013-10-31 11:55:59 +0100

    license

--- bconf.go
+++ bconf.go
@@ -1,3 +1,6 @@
+// Copyright 2013 Artur Grabowski. All rights reserved.
+// Use of this source code is governed by a ISC-style
+// license that can be found in the LICENSE file.
 package bconf
 
 import (

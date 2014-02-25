2a7025c638184f4a1c5987c143c6f63b4afce4bb
Artur Grabowski <art@blahonga.org>
2014-02-25 13:35:38 +0100

    Code for getting bconf from http.
    This has also been sitting around in my tree for a while. Status unknown, but it compiles.

---
+++ bconf_http.go
@@ -0,0 +1,31 @@
+// Copyright 2013 Artur Grabowski. All rights reserved.
+// Use of this source code is governed by a ISC-style
+// license that can be found in the LICENSE file.
+package bconf
+
+import (
+	"net/http"
+	"net/url"
+	"errors"
+	"fmt"
+)
+
+func (bc *Bconf) LoadHTTP(bconfurl, host, appl string) error {
+	v := url.Values{}
+	if host != "" {
+		v.Set("host", host)
+	}
+	if appl != "" {
+		v.Set("appl", appl)
+	}
+	res, err := http.PostForm(bconfurl, v)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("bconf.LoadHTTP - response: %v", res.Status))
+	}
+	bc.LoadJSONReader(res.Body)
+	res.Body.Close()
+	return nil
+}
